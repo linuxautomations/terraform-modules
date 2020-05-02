e650619519d3b1cf1f91072d13ba9507a0ff7406
KalluruS <>
2020-05-02 14:59:58 +0530

    Adding files

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,18 +1,20 @@
 resource "aws_subnet" "private" {
-  count         = 2
-  cidr_block    = cidrsubnet(var.VPC_CIDR, 10, count.index)
-  vpc_id        = aws_vpc.default.id
-  tags          = {
-    Name        = "${local.subnet_prefix}-private-${count.index+1}"
+  count               = 2
+  cidr_block          = cidrsubnet(var.VPC_CIDR, 10, count.index)
+  vpc_id              = aws_vpc.default.id
+  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
+  tags                = {
+    Name              = "${local.subnet_prefix}-private-${count.index+1}"
   }
 }
 
 resource "aws_subnet" "public" {
-  count         = 2
-  cidr_block    = cidrsubnet(var.VPC_CIDR, 10, count.index+2)
-  vpc_id        = aws_vpc.default.id
-  tags          = {
-    Name        = "${local.subnet_prefix}-public-${count.index+1}"
+  count               = 2
+  cidr_block          = cidrsubnet(var.VPC_CIDR, 10, count.index+2)
+  vpc_id              = aws_vpc.default.id
+  availability_zone   = element(data.aws_availability_zones.available.names, count.index)
+  tags                = {
+    Name              = "${local.subnet_prefix}-public-${count.index+1}"
   }
 }
-q
+
