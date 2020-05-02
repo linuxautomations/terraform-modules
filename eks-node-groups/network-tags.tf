6802bae83ef12bba14a1f570526eb2cef661a2b1
KalluruS <>
2020-05-02 21:34:27 +0530

    Adding files

--- eks-node-groups/network-tags.tf
+++ eks-node-groups/network-tags.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "network" {
+  triggers {
+    build = timestamp()
+  }
   provisioner "local-exec" {
     command = "aws ec2 create-tags --resources ${local.TAGS_SUBNETS} --tags Key=kubernetes.io/cluster/${local.EKS_NAME},Value=shared --region ${data.aws_region.current.name}"
   }
