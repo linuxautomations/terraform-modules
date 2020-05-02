f120c34f38c971225d180ff9152a4b7da16d8d2c
KalluruS <>
2020-05-02 15:47:28 +0530

    Adding files

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -25,3 +25,15 @@ resource "aws_route_table" "private" {
     Name          = "private"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  count           = 2
+  subnet_id       = element(aws_subnet.public.*.id, count.index)
+  route_table_id  = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count           = 2
+  subnet_id       = element(aws_subnet.private.*.id, count.index)
+  route_table_id  = aws_route_table.private.id
+}
